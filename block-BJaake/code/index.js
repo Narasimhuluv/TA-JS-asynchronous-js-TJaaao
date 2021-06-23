3eeb331648a8200a321a5e13e7332a2d853b8ee6
Narasimhuluv <vasamnarasimhulu123@gmail.com>
2021-06-23 11:12:47 +0530

    added files

--- block-BJaake/code/index.js
+++ block-BJaake/code/index.js
@@ -1,29 +1,68 @@
-let source = document.getElementById('source');
-function fetch(url) {
-  return new Promise((resolve, reject) => {
-    let xhr = new XMLHttpRequest();
-    xhr.open('GET', url);
-    // let newsData = JSON.parse(xhr.response);
-    xhr.onload = function () {
-      let newsData = resolve(JSON.parse(xhr.response));
-    };
-    xhr.onerror = function () {
-      reject('error');
-    };
-    xhr.send();
-  })
-    .then((newsData) => {
-      createUI(newsData);
-      console.log(newsData);
-    })
-    .catch((error) => console.log(error));
-}
-source.addEventListener('change', fetch);
-fetch(`https://api.spaceflightnewsapi.net/v3/articles?_limit=30`);
+// let source = document.getElementById('source');
+// function fetch(url) {
+//   return new Promise((resolve, reject) => {
+//     let xhr = new XMLHttpRequest();
+//     xhr.open('GET', url);
+//     // let newsData = JSON.parse(xhr.response);
+//     xhr.onload = function () {
+//       let newsData = resolve(JSON.parse(xhr.response));
+//     };
+//     xhr.onerror = function () {
+//       reject('error');
+//     };
+//     xhr.send();
+//   })
+//     .then((newsData) => {
+//       createUI(newsData);
+//       console.log(newsData);
+//     })
+//     .catch((error) => console.log(error));
+// }
+// source.addEventListener('change', fetch);
+// fetch(`https://api.spaceflightnewsapi.net/v3/articles?_limit=30`);
 
+// let UnorderdList = document.querySelector('ul');
+// function createUI(data, event) {
+//   data.forEach((element, event) => {
+//     let list = document.createElement('li');
+//     list.classList.add('list');
+
+//     let articleImage = document.createElement('img');
+//     articleImage.classList.add('articleImage');
+//     articleImage.src = element.imageUrl;
+
+//     let titleNews = document.createElement('h2');
+//     titleNews.classList.add('titleNews');
+//     titleNews.innerHTML = element.title;
+
+//     let news = document.createElement('p');
+//     news.innerHTML = element.newsSite;
+//     news.style.marginTop = '20px';
+
+//     list.append(articleImage, titleNews, news);
+//     UnorderdList.append(list);
+//     console.log(element.newsSite);
+//   });
+// }
+
+let url = `https://api.spaceflightnewsapi.net/v3/articles?_limit=30`;
 let UnorderdList = document.querySelector('ul');
-function createUI(data, event) {
-  data.forEach((element, event) => {
+let allNews = [];
+
+fetch(url)
+  .then((res) => res.json())
+  .then((news) => {
+    createUI(news);
+    allNews = news;
+    let allSites = Array.from(
+      new Set(news.map((eachNews) => eachNews.newsSite))
+    );
+    console.log(allSites);
+  });
+
+function createUI(data) {
+  UnorderdList.innerHTML = '';
+  data.forEach((element) => {
     let list = document.createElement('li');
     list.classList.add('list');
 
@@ -31,27 +70,36 @@ function createUI(data, event) {
     articleImage.classList.add('articleImage');
     articleImage.src = element.imageUrl;
 
+    let dataDiv = document.createElement('div');
+    dataDiv.classList.add('dataDiv');
+
+    let NDTV = document.createElement('p');
+    NDTV.innerHTML = element.newsSite;
+    NDTV.classList.add('ndtv');
+
     let titleNews = document.createElement('h2');
     titleNews.classList.add('titleNews');
     titleNews.innerHTML = element.title;
 
-    news = document.createElement('p');
-    news.innerHTML = element.newsSite;
-    news.style.marginTop = '20px';
+    let readMore = document.createElement('button');
+    readMore.classList.add('readMore');
+    readMore.innerHTML = 'Read More';
 
-    list.append(articleImage, titleNews, news);
     UnorderdList.append(list);
-    console.log(element.newsSite);
+    list.append(articleImage, dataDiv);
+    dataDiv.append(NDTV, titleNews, readMore);
   });
 }
 
-// function fetch(url) {
-//   let xhr = new XMLHttpRequest();
-//   xhr.open('GET', url);
-//   xhr.onload = function () {
-//     let dataNews = JSON.parse(xhr.response);
-//     console.log(dataNews);
-//   };
-//   xhr.send();
-// }
-// fetch(`https://api.spaceflightnewsapi.net/v3/articles?_limit=30`);
+let source = document.getElementById('source');
+source.addEventListener('change', (event) => {
+  console.log(event.target.value);
+  let fileterNews = allNews.filter(
+    (news) => news.newsSite === event.target.value
+  );
+  if (event.target.value == '') {
+    fileterNews = allNews;
+  }
+  createUI(fileterNews);
+  console.log(fileterNews);
+});
