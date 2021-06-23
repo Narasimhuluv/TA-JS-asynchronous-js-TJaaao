d4024cd2d4aaaf581c0a9d9c0e02eec9a48af130
Narasimhuluv <vasamnarasimhulu123@gmail.com>
2021-06-23 12:46:23 +0530

    added files

---
+++ block-BJaajw/code/index.js
@@ -0,0 +1,113 @@
+// let source = document.getElementById('source');
+// function fetch(url) {
+//   return new Promise((resolve, reject) => {
+//     let xhr = new XMLHttpRequest();
+//     xhr.open('GET', url);
+//     // let newsData = JSON.parse(xhr.response);
+//     xhr.onload = function () {
+//       let newsData = resolve(JSON.parse(xhr.response));
+//     };
+//     xhr.onerror = function () {
+//       reject('error');
+//     };
+//     xhr.send();
+//   })
+//     .then((newsData) => {
+//       createUI(newsData);
+//       console.log(newsData);
+//     })
+//     .catch((error) => console.log(error));
+// }
+// source.addEventListener('change', fetch);
+// fetch(`https://api.spaceflightnewsapi.net/v3/articles?_limit=30`);
+
+// let UnorderdList = document.querySelector('ul');
+// function createUI(data, event) {
+//   data.forEach((element, event) => {
+//     let list = document.createElement('li');
+//     list.classList.add('list');
+
+//     let articleImage = document.createElement('img');
+//     articleImage.classList.add('articleImage');
+//     articleImage.src = element.imageUrl;
+
+//     let titleNews = document.createElement('h2');
+//     titleNews.classList.add('titleNews');
+//     titleNews.innerHTML = element.title;
+
+//     let news = document.createElement('p');
+//     news.innerHTML = element.newsSite;
+//     news.style.marginTop = '20px';
+
+//     list.append(articleImage, titleNews, news);
+//     UnorderdList.append(list);
+//     console.log(element.newsSite);
+//   });
+// }
+
+let url = `https://api.spaceflightnewsapi.net/v3/articles?_limit=30`;
+let UnorderdList = document.querySelector('ul');
+let allNews = [];
+
+fetch(url)
+    .then((res) => {
+        if (!res.ok) {
+          throw new error(`Error Happend : ${res.status}`)
+        }
+        return res.json()
+  })
+  .then((news) => {
+    createUI(news);
+    allNews = news;
+    let allSites = Array.from(
+      new Set(news.map((eachNews) => eachNews.newsSite))
+    );
+    console.log(allSites);
+  }).catch((error) => {
+      UnorderdList.innerHTML = error
+  })
+
+function createUI(data) {
+  UnorderdList.innerHTML = '';
+  data.forEach((element) => {
+    let list = document.createElement('li');
+    list.classList.add('list');
+
+    let articleImage = document.createElement('img');
+    articleImage.classList.add('articleImage');
+    articleImage.src = element.imageUrl;
+
+    let dataDiv = document.createElement('div');
+    dataDiv.classList.add('dataDiv');
+
+    let NDTV = document.createElement('p');
+    NDTV.innerHTML = element.newsSite;
+    NDTV.classList.add('ndtv');
+
+    let titleNews = document.createElement('h2');
+    titleNews.classList.add('titleNews');
+    titleNews.innerHTML = element.title;
+
+    let readMore = document.createElement('button');
+    readMore.classList.add('readMore');
+    readMore.innerHTML = 'Read More';
+
+    UnorderdList.append(list);
+    list.append(articleImage, dataDiv);
+    dataDiv.append(NDTV, titleNews, readMore);
+  });
+}
+
+let source = document.getElementById('source');
+source.addEventListener('change', (event) => {
+  console.log(event.target.value);
+  let fileterNews = allNews.filter(
+    (news) => news.newsSite === event.target.value
+  );
+  if (event.target.value == '') {
+    fileterNews = allNews;
+  }
+
+  createUI(fileterNews);
+  console.log(fileterNews);
+});
